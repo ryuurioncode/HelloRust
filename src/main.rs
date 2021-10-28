6e4582671e0e38836252d0033c1f76977ca61c37
ryuu <ryuucode@ya.ru>
2021-10-28 08:02:41 +0300

    Implemented socket binance btc and eth tracking.

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,77 @@
-use binance::api::*;
-use binance::market::*;
-use binance::futures::model::KlineSummary;
+use std::thread;
+use std::time;
 
+use websocket::client::ClientBuilder;
+use websocket::{Message, OwnedMessage};
+
+
+const CONNECTION: &'static str = "wss://stream.binance.com:9443/ws";
+const targets: [&str; 2] = ["btcusdt", "ethusdt"]; // more then 1 in stream couses sequence bug
+const update_time_ms: i32 = 1000; // 1000 ms or 100 ms
+const depth: i32 = 5; // Valid are 5, 10, or 20.
+const life_time_sec: u64 = 10;
 fn main() {
-    let market: Market = Binance::new(None, None);
-
-    // Order book at default depth
-    match market.get_depth("BNBETH") {
-        Ok(answer) => println!("{:?}", answer),
-        Err(e) => println!("Error: {}", e),
+    let mut client = ClientBuilder::new(CONNECTION)
+        .unwrap()
+        // .connect_insecure()
+        .connect(None)
+        .unwrap();
+	println!("Successfully connected");
+    {
+        let mut message: String = r#"{"method": "SUBSCRIBE","params":["#.to_owned();
+        for i in 0..targets.len() {
+            message.push_str("\"");
+            message.push_str(targets[i]);
+            message.push_str("@depth");
+            message.push_str(&depth.to_string());
+            message.push_str("@");
+            message.push_str(&update_time_ms.to_string());
+            message.push_str("ms\"");
+            if i != (targets.len() - 1) {
+                message.push_str(",");
+            }
+        }
+        message.push_str(r#"],"id":1}"#);
+        client.send_message(&OwnedMessage::Text(message.to_string()));
     }
-
-    // Order book at depth 500
-    match market.get_custom_depth("BNBETH", 500) {
-        Ok(answer) => println!("{:?}", answer),
-        Err(e) => println!("Error: {}", e),
+    {
+        let message = client.recv_message();
     }
-
-    // Latest price for ALL symbols
-    match market.get_all_prices() {
-        Ok(answer) => println!("{:?}", answer),
-        Err(e) => println!("Error: {:?}", e),
-    }
-
-    // Latest price for ONE symbol
-    match market.get_price("BNBETH") {
-        Ok(answer) => println!("{:?}", answer),
-        Err(e) => println!("Error: {:?}", e),
-    }
-
-    // Current average price for ONE symbol
-    match market.get_average_price("BNBETH") {
-        Ok(answer) => println!("{:?}", answer),
-        Err(e) => println!("Error: {:?}", e),
-    }
-
-    // Best price/qty on the order book for ALL symbols
-    match market.get_all_book_tickers() {
-        Ok(answer) => println!("{:?}", answer),
-        Err(e) => println!("Error: {:?}", e),
-    }
-
-    // Best price/qty on the order book for ONE symbol
-    match market.get_book_ticker("BNBETH") {
-        Ok(answer) => println!(
-            "Bid Price: {}, Ask Price: {}",
-            answer.bid_price, answer.ask_price
-        ),
-        Err(e) => println!("Error: {:?}", e),
-    }
-
-    // 24hr ticker price change statistics
-    match market.get_24h_price_stats("BNBETH") {
-        Ok(answer) => println!(
-            "Open Price: {}, Higher Price: {}, Lower Price: {:?}",
-            answer.open_price, answer.high_price, answer.low_price
-        ),
-        Err(e) => println!("Error: {:?}", e),
-    }
-
-    // last 10 5min klines (candlesticks) for a symbol:
-    match market.get_klines("BNBETH", "5m", 10, None, None) {
-        Ok(klines) => {   
-            match klines {
-                binance::model::KlineSummaries::AllKlineSummaries(klines) => {
-                    let kline: KlineSummary = klines[0].clone(); // You need to iterate over the klines
-                    println!(
-                        "Open: {}, High: {}, Low: {}",
-                        kline.open, kline.high, kline.low
-                    )
+    let receive_loop = thread::spawn(move || {
+        let mut targetsIt: i32 = 0;
+        let targetsItPtr = &mut targetsIt;
+        loop {
+            let message = client.recv_message();
+            let message = match message {
+                Ok(m) => m,
+                Err(e) => {
+                    println!("Receive Loop: {:?}", e);
+                    return;
+                }
+            };
+            match message {
+                OwnedMessage::Close(_) => {
+                    client.send_message(&OwnedMessage::Close(None));
+                    return;
+                }
+                OwnedMessage::Ping(data) => {
+                    client.send_message(&OwnedMessage::Pong(data));
+                }
+                OwnedMessage::Text(value) => {
+                    let parsed = json::parse(&value).unwrap();
+                    if !(parsed["bids"][0][0].is_null() && parsed["asks"][0][0].is_null()) {
+                        *targetsItPtr = (*targetsItPtr + 1) % (targets.len() as i32);
+                        println!("[{:?}] OB -> Binance : {} {} x {}", chrono::offset::Utc::now(), targets[*targetsItPtr as usize], parsed["bids"][0][0], parsed["asks"][0][0]);
+                    } else {
+                        println!("[{:?}] OB -> Binance : error parse : {:?}", chrono::offset::Utc::now(), value);
+                    }
+                }
+                _ => {
+                    println!("[{:?}] OB -> Binance : {:?}", chrono::offset::Utc::now(), message);
                 }
             }
-        },
-        Err(e) => println!("Error: {}", e),
-    }
+        }
+    });
+    let tsleep = time::Duration::from_millis(life_time_sec * 1000);
+    thread::sleep(tsleep);
 }
